34e69c418670ce2e556d9f5d8e61055d53f93fdd
Mahalaxmi-Komuravelly <mahalaxmikomuravelly@gmail.com>
2026-03-15 16:13:06 +0530

    add routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Signup from './pages/Signup'
+import Login from './pages/Login'
+import ProtectedRoute from './component/ProtectedRoute'
+import Dashboard from './pages/Dashboard'
+import MoneyTransfer from './pages/MoneyTransfer'
+import AccountStatement from './pages/AccountStatement'
 
 const App = () => {
   return (
     <div>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Signup/>}/>
+          <Route path='/login' element={<Login/>}/>
+
+          <Route path='/dashboard' element={
+            <ProtectedRoute>
+              <Dashboard/>
+            </ProtectedRoute>
+          }/>
+
+          <Route path='/money-transfer' element={
+            <ProtectedRoute>
+              <MoneyTransfer/>
+            </ProtectedRoute>
+          }/>
+
+          <Route path='/account-statement' element={
+            <ProtectedRoute>
+              <AccountStatement/>
+            </ProtectedRoute>
+          }/>
+
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
